0b45a581f1d7f9d7788dc5408e88c6a0f1a3cc05
guest1 <mamasydykova_g@auca.kg>
2021-10-27 19:32:01 +0600

    second commit

---
+++ app/src/main/java/kg/Zara07/twitter/CostomActivity.kt
@@ -0,0 +1,11 @@
+package kg.Zara07.twitter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CostomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.costom)
+    }
+}
